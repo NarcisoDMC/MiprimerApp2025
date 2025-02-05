ef87a131b6a97638ac37dcaa3d5cb68e28333a77
NarcisoDMC <davidmadefukingtroll810@gmail.com>
2025-02-05 00:16:27 -0600

    Nuevo cambio

---
+++ app/src/main/java/com/example/miprimerapp2025/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.miprimerapp2025
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.miprimerapp2025.ui.theme.MiprimerApp2025Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MiprimerApp2025Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    Button(
+                        onClick = {
+                            println("Hola mundo Narciso")
+                        }, modifier = Modifier.padding(16.dp)) {
+                            Text("Saludar")
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MiprimerApp2025Theme {
+        Greeting("Android Narciso")
+        Button(
+            onClick = {
+                println("Hola Mundo")
+            },modifier = Modifier.padding(16.dp)) {
+                Text("saludar")
+        }
+    }
+}
