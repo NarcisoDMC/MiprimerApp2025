90edd577b4ef8b33bce3b688ba2ae93110daa325
NarcisoDMC <davidmadefukingtroll810@gmail.com>
2025-02-18 16:18:29 -0600

    Modificaciones para la clase del dia 18 de febrero del 2025

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,16 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,7 +46,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                content()
+                Backimage()
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ){
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ){
+                        TextField1()
+                        Spacer(modifier = Modifier.height(12.dp))
+                        TextField2()
+                        Spacer(modifier = Modifier.height(18.dp))
+                        ButtonCalculate()
+                    }
+                }
+
             }
         }
     }
@@ -34,13 +70,48 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun content(){
-    Text(
-        text="Hola Jetpack compose",
-        color = Color.Red,
-        fontWeight = FontWeight.Bold,
-        fontSize = 20.sp,
-        textAlign = TextAlign.Center,
-        modifier = Modifier.padding().background(Color.Blue)
+fun Backimage(){
+    Image(
+        painter = painterResource(id = R.drawable.ic_mustang),
+        contentDescription = "Mustang background",
+        modifier = Modifier.fillMaxWidth()
     )
 }
+
+@Composable
+fun TextField1() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        placeholder = { Text(text = "Valor 1") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = Modifier.fillMaxWidth(0.8f)
+    )
+}
+
+@Composable
+fun TextField2 (){
+    var text by remember { mutableStateOf(TextFieldValue(""))}
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        placeholder = { Text(text = "Valor 2")},
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = Modifier.fillMaxWidth(0.8f)
+    )
+}
+
+@Composable
+fun ButtonCalculate(){
+    Button(onClick = {
+
+    }) {
+        Text(text = "Calcular")
+    }
+}
+
